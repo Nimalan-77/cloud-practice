3f1cf97bcdc77de6d88ddd3bcacc3e96263f2500
Nimalan <nimalan1940@gmail.com>
2026-04-21 16:34:54 +0530

    Jenkinfile updated

--- jenkins-gke-project/app.py
+++ jenkins-gke-project/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello from GKE!</h1><p>This was deployed via Jenkins using WIF!</p>"
+    return "<h1>Hello from GKE!</h1><br><p>This was deployed via Jenkins using WIF!</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
