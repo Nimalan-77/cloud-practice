78cabe701b6a282a270ca5d4f9817feaea927cdd
Nimalan <nimalan1940@gmail.com>
2026-04-24 18:46:38 +0530

    New main file

--- my-gitops-project/app/main.py
+++ my-gitops-project/app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     # We use an environment variable so we can see if the deployment actually updated
     version = os.getenv("APP_VERSION", "1.0.0")
-    return f"Hello from GKE Autopilot! Current Version: {version}"
+    return f"Hello from GKE Autopilot using ArgoCD Current Version: {version}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
